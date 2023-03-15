26ba36a33ef4c18c2e732b642edf3d58c6e73674
maskalter <127936528+maskalter@users.noreply.github.com>
2023-03-16 00:51:00 +0800

    Add files via upload

--- indexJS.js
+++ indexJS.js
@@ -1,6 +1,12 @@
+var Language = 1;
+
 $(document).ready(function()
 {
     WindowSize();
+
+    $("#L_CH").click(function(){ Language = 1; alert("此功能開發中，敬請期待！");});
+    $("#L_JP").click(function(){ Language = 2; alert("此功能開發中，敬請期待！");});
+    $("#L_EN").click(function(){ Language = 3; alert("此功能開發中，敬請期待！");});
 });
 
 $(window).resize(function() 
@@ -10,13 +16,36 @@ $(window).resize(function()
 
 function WindowSize()
 {
-    if($(window).width() < 800)
+    var WH = $(window).height();
+    var WW = $(window).width();
+
+    if( WH/WW > 16/11 )
     {
         $("#Content").css("width", $(window).width());
+        $("#Nav").css("right", $(window).width());
+        $("#Avatar").css("width",  $(window).width()*0.2);
+        $("#Avatar").css("height", $(window).width()*0.2);
+    }
+    else if( WH/WW < 16/11 && WW < 1000 )
+    {
+        $("#Content").css({"-webkit-transform":"translate(0%, 0%)"});
+        $("#Content").css("left",  $(window).width()*0.2);
+        $("#Content").css("width", $(window).width()*0.8);
+        $("#Nav").css("right", $(window).width()*0.8);
+        $("#Nav").css("width", $(window).width()*0.2);
     }
     else
     {
-        $("#Content").css("width", 800);
+        $("#Content").css("width", 600);
+        $("#Nav").css("right", $(window).width()/2+300);
     }
-    alert($(window).width());
+
+    $("#Shop").css("width",  $("#Content").width()*0.9);
+    $("#Shop").css("height", $("#Content").width()*0.4);
+    $("#Shop").css({"border-radius": $("#Shop").height()*0.1});
+    $(".Shop_But").css("width",  $("#Content").width()*0.6*0.3);
+    $(".Shop_But").css("height", $("#Content").width()*0.6*0.3);
+    $(".LinkBox").css({"border-radius": $("#Shop").height()*0.1});
+
+    //alert(WH/WW);
 }
