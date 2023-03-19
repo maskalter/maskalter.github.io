691b65bbb3ee7057ed6737ce35710ee15c61dd8d
maskalter <127936528+maskalter@users.noreply.github.com>
2023-03-19 14:12:54 +0800

    Add files via upload

--- indexJS.js
+++ indexJS.js
@@ -4,9 +4,9 @@ $(document).ready(function()
 {
     WindowSize();
 
-    $("#L_CH").click(function(){ Language = 1; alert("此功能開發中，敬請期待！");});
-    $("#L_JP").click(function(){ Language = 2; alert("此功能開發中，敬請期待！");});
-    $("#L_EN").click(function(){ Language = 3; alert("此功能開發中，敬請期待！");});
+    $("#L_CH").click(function(){ Language = 1; LanguageSelection();});
+    $("#L_JP").click(function(){ Language = 2; LanguageSelection();});
+    $("#L_EN").click(function(){ Language = 3; LanguageSelection();});
 });
 
 $(window).resize(function() 
@@ -23,8 +23,15 @@ function WindowSize()
     {
         $("#Content").css("width", $(window).width());
         $("#Nav").css("right", $(window).width());
-        $("#Avatar").css("width",  $(window).width()*0.2);
-        $("#Avatar").css("height", $(window).width()*0.2);
+        $(".Avatar").css("width",  $(window).width()*0.2);
+        $(".Avatar").css("height", $(window).width()*0.2);
+
+        $("body").css({"font-size":"36px"});
+        $("p"   ).css({"font-size":"36px"});
+        $("h2"  ).css({"font-size":"48px"});
+        $("h3"  ).css({"font-size":"42px"});
+        $("h5"  ).css({"font-size":"36px"});
+        $(".LinkTitle").css({"padding-left":"1.75em"});
     }
     else if( WH/WW < 16/11 && WW < 1000 )
     {
@@ -33,19 +40,55 @@ function WindowSize()
         $("#Content").css("width", $(window).width()*0.8);
         $("#Nav").css("right", $(window).width()*0.8);
         $("#Nav").css("width", $(window).width()*0.2);
+
+        $("body").css({"font-size":"16px"});
+        $("p"   ).css({"font-size":"16px"});
+        $("h2"  ).css({"font-size":"24px"});
+        $("h3"  ).css({"font-size":"20px"});
+        $("h5"  ).css({"font-size":"16px"});
+        $(".LinkTitle").css({"padding-left":"2.75em"});
     }
     else
     {
         $("#Content").css("width", 600);
         $("#Nav").css("right", $(window).width()/2+300);
+
+        $("body").css({"font-size":"16px"});
+        $("p"   ).css({"font-size":"16px"});
+        $("h2"  ).css({"font-size":"24px"});
+        $("h3"  ).css({"font-size":"20px"});
+        $("h5"  ).css({"font-size":"16px"});
+        $(".LinkTitle").css({"padding-left":"2.75em"});
     }
 
-    $("#Shop").css("width",  $("#Content").width()*0.9);
-    $("#Shop").css("height", $("#Content").width()*0.4);
-    $("#Shop").css({"border-radius": $("#Shop").height()*0.1});
+    $(".Shop").css("width",  $("#Content").width()*0.9);
+    $(".Shop").css("height", $("#Content").width()*0.4);
+    $(".Shop").css({"border-radius": $(".Shop").height()*0.1});
     $(".Shop_But").css("width",  $("#Content").width()*0.6*0.3);
     $(".Shop_But").css("height", $("#Content").width()*0.6*0.3);
-    $(".LinkBox").css({"border-radius": $("#Shop").height()*0.1});
+    $(".LinkBox").css({"border-radius": $(".Shop").height()*0.1});
 
     //alert(WH/WW);
+}
+
+function LanguageSelection()
+{
+    if(Language == 1)
+    {
+        $("#CH").css({"display":"block"});
+        $("#JP").css({"display":"none" });
+        $("#EN").css({"display":"none" });
+    }
+    else if(Language == 2)
+    {
+        $("#CH").css({"display":"none" });
+        $("#JP").css({"display":"block"});
+        $("#EN").css({"display":"none" });
+    }
+    else if(Language == 3)
+    {
+        $("#CH").css({"display":"none" });
+        $("#JP").css({"display":"none" });
+        $("#EN").css({"display":"block"});
+    }
 }
